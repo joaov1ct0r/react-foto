27315fb767952c73dec251849125376e5595447d
Joao Victor <jov9051@outlook.com>
2022-02-06 18:09:10 -0300

    codigo de section contato movido para componente SectionContato.js

--- src/components/SectionContato.js
+++ src/components/SectionContato.js
@@ -1,1 +1,21 @@
-function SectionContato() {}
+function SectionContato() {
+    return (
+        <section className="contato">
+            <h2>Contato</h2>
+            <p>Entre em contato conosco.</p>
+            <div>
+                <img src="../assets/img/fone.png" alt="fone"></img>
+                <p>
+                    <a href="tel:xx55555555">(xx) 5555-5555</a>
+                </p>
+            </div>
+            <div>
+                <img src="../assets/img/contato.png" alt="email"></img>
+
+                <p>
+                    <a href="mailto:email@contato.com">email@contato.com</a>
+                </p>
+            </div>
+        </section>
+    );
+}
