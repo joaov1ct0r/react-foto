9c8e19d1198c5d6b169bb6d76ab7b53145a17b35
Joao Victor <jov9051@outlook.com>
2022-02-06 16:26:36 -0300

    codigo header movido para componente Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,1 +1,16 @@
-function Header() {}
+function Header() {
+    return (
+        <div className="header">
+            <header>
+                <h1>Lorem Ipsum</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Debitis exercitationem officiis.
+                </p>
+                <button type="submit" href="#sobre" className="botao">
+                    Ver Mais
+                </button>
+            </header>
+        </div>
+    );
+}
