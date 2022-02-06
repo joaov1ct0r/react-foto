256faab078bf5a245b4619fdac9887a7f84af444
Joao Victor <jov9051@outlook.com>
2022-02-06 16:40:51 -0300

    codigo section sobre movido para componente SectionSobre.js

--- src/components/SectionSobre.js
+++ src/components/SectionSobre.js
@@ -1,1 +1,14 @@
-function SectionSobre() {}
+function SectionSobre() {
+    return (
+        <section className="sobre">
+            <h2>Sobre</h2>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Debitis exercitationem officiis.
+            </p>
+            <button href="#contato" className="botao">
+                Entre em contato
+            </button>
+        </section>
+    );
+}
