eee3455d943f92a6056cc28a773f50a257ca1e14
Joao Victor <jov9051@outlook.com>
2022-02-06 17:11:51 -0300

    codigo de section portfolio movido para componente SectionPortfolio

--- src/components/SectionPortfolio.js
+++ src/components/SectionPortfolio.js
@@ -1,1 +1,25 @@
-function SectionPortfolio() {}
+function SectionPortfolio() {
+    return (
+        <section ClassName="portfolio">
+            <h2>Portfólio</h2>
+            <img src="../assets/img/portfolio-1.jpg" alt="cogumelos"></img>
+            <img
+                src="../assets/img/portfolio-2.jpeg"
+                alt="Sol no horizonte"
+            ></img>
+            <img
+                src="../assets/img/portfolio-3.jpeg"
+                alt="Pessoas á beira de vale"
+            ></img>
+            <img
+                src="../assets/img/portfolio-4.jpg"
+                alt="Floresta nevada"
+            ></img>
+            <img src="../assets/img/portfolio-5.jpeg" alt="Praia deserta"></img>
+            <img
+                src="../assets/img/portfolio-6.jpeg"
+                alt="Alce á beira do rio"
+            ></img>
+        </section>
+    );
+}
