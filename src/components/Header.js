a2e9351198db0aae4c7a36fec129a7791b219542
Joao Victor <jov9051@outlook.com>
2022-02-06 16:41:44 -0300

    className removido do componente Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,15 @@
 function Header() {
     return (
-        <div className="header">
-            <header>
-                <h1>Lorem Ipsum</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Debitis exercitationem officiis.
-                </p>
-                <button type="submit" href="#sobre" className="botao">
-                    Ver Mais
-                </button>
-            </header>
-        </div>
+        <header>
+            <h1>Lorem Ipsum</h1>
+            <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Debitis exercitationem officiis.
+            </p>
+            <button type="submit" href="#sobre" className="botao">
+                Ver Mais
+            </button>
+        </header>
     );
 }
 
