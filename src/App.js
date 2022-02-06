2fa6060d38cfbf809aa19a736c15747c70cebfc7
Joao Victor <jov9051@outlook.com>
2022-02-06 17:00:26 -0300

    SectionServicos declarado em app.js

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Header from './components/Header';
 
 import SectionSobre from './components/SectionSobre';
 
+import SectionServicos from './components/SectionServicos';
+
 function App() {
     return (
         <div className="App">
