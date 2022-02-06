ba8f73f07f9f9b5ff1b8800cdf86f63049aa92ba
Joao Victor <jov9051@outlook.com>
2022-02-06 16:42:27 -0300

    codigo sectionSobre removido de app.js

--- src/App.js
+++ src/App.js
@@ -9,16 +9,6 @@ function App() {
         <div className="App">
             <Nav></Nav>
             <Header></Header>
-            <section className="sobre">
-                <h2>Sobre</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Debitis exercitationem officiis.
-                </p>
-                <button href="#contato" className="botao">
-                    Entre em contato
-                </button>
-            </section>
         </div>
     );
 }
