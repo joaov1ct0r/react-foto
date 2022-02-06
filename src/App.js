f2486eb28c6c3565008028d03b9f27654ea0b35b
Joao Victor <jov9051@outlook.com>
2022-02-06 16:14:27 -0300

    space

--- src/App.js
+++ src/App.js
@@ -10,12 +10,15 @@ function App() {
                     <li>
                         <a href="#sobre">Sobre</a>
                     </li>
+
                     <li>
                         <a href="#servicos">Serviços</a>
                     </li>
+
                     <li>
                         <a href="#portfolio">Portfólio</a>
                     </li>
+
                     <li>
                         <a href="#contato">Contato</a>
                     </li>
