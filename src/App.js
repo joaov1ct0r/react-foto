e3c40dcde41898a65adb460e4b311555b7f18449
Joao Victor <jov9051@outlook.com>
2022-02-06 17:12:29 -0300

    codigo de section portfolio removido de app.js

--- src/App.js
+++ src/App.js
@@ -15,30 +15,6 @@ function App() {
             <Header></Header>
             <SectionSobre></SectionSobre>
             <SectionServicos></SectionServicos>
-            <section ClassName="portfolio">
-                <h2>Portfólio</h2>
-                <img src="../assets/img/portfolio-1.jpg" alt="cogumelos"></img>
-                <img
-                    src="../assets/img/portfolio-2.jpeg"
-                    alt="Sol no horizonte"
-                ></img>
-                <img
-                    src="../assets/img/portfolio-3.jpeg"
-                    alt="Pessoas á beira de vale"
-                ></img>
-                <img
-                    src="../assets/img/portfolio-4.jpg"
-                    alt="Floresta nevada"
-                ></img>
-                <img
-                    src="../assets/img/portfolio-5.jpeg"
-                    alt="Praia deserta"
-                ></img>
-                <img
-                    src="../assets/img/portfolio-6.jpeg"
-                    alt="Alce á beira do rio"
-                ></img>
-            </section>
         </div>
     );
 }
