dba30737628683bccff106068e5347d8120511a3
Joao Victor <jov9051@outlook.com>
2022-02-06 16:42:07 -0300

    className removido do componente Nav.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,28 +1,26 @@
 function Nav() {
     return (
-        <div className="nav">
-            <nav>
-                <a href="https://localhost:3000">Lorem Ipsum</a>
+        <nav>
+            <a href="https://localhost:3000">Lorem Ipsum</a>
 
-                <ul>
-                    <li>
-                        <a href="#sobre">Sobre</a>
-                    </li>
+            <ul>
+                <li>
+                    <a href="#sobre">Sobre</a>
+                </li>
 
-                    <li>
-                        <a href="#servicos">Serviços</a>
-                    </li>
+                <li>
+                    <a href="#servicos">Serviços</a>
+                </li>
 
-                    <li>
-                        <a href="#portfolio">Portfólio</a>
-                    </li>
+                <li>
+                    <a href="#portfolio">Portfólio</a>
+                </li>
 
-                    <li>
-                        <a href="#contato">Contato</a>
-                    </li>
-                </ul>
-            </nav>
-        </div>
+                <li>
+                    <a href="#contato">Contato</a>
+                </li>
+            </ul>
+        </nav>
     );
 }
 
