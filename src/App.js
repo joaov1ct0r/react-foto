79d49b2531138625924e9b1d575b436838c8ca61
Joao Victor <jov9051@outlook.com>
2022-02-06 16:27:45 -0300

    codigo header removido de app.js

--- src/App.js
+++ src/App.js
@@ -6,16 +6,6 @@ function App() {
     return (
         <div className="App">
             <Nav></Nav>
-            <header>
-                <h1>Lorem Ipsum</h1>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-                    Debitis exercitationem officiis.
-                </p>
-                <button type="submit" href="#sobre" className="botao">
-                    Ver Mais
-                </button>
-            </header>
         </div>
     );
 }
