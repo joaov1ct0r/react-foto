8576dad0f425ad53ef4d88213596270d41b30e55
Joao Victor <jov9051@outlook.com>
2022-02-06 17:00:53 -0300

    SectionServicos sendo utilizado em app.js

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ function App() {
             <Nav></Nav>
             <Header></Header>
             <SectionSobre></SectionSobre>
+            <SectionServicos></SectionServicos>
         </div>
     );
 }
