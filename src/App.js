b0758ec88175ba2b50e1f43f0003f2bbd67bcec1
Joao Victor <jov9051@outlook.com>
2022-02-06 16:22:28 -0300

    elemento p criado dentro de header em app.js

--- src/App.js
+++ src/App.js
@@ -8,6 +8,10 @@ function App() {
             <Nav></Nav>
             <header>
                 <h1>Lorem Ipsum</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                    Debitis exercitationem officiis.
+                </p>
             </header>
         </div>
     );
