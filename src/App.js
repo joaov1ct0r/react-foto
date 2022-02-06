a01677b166e9da425b9f91d88086e972342fc126
Joao Victor <jov9051@outlook.com>
2022-02-06 16:42:53 -0300

    componente SectionSobre declarado em app.js

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import Nav from './components/Nav';
 
 import Header from './components/Header';
 
+import SectionSobre from './components/SectionSobre';
+
 function App() {
     return (
         <div className="App">
