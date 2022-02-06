448e89864fd0aa131ac3db69f3d285b5d5eba811
Joao Victor <jov9051@outlook.com>
2022-02-06 16:17:34 -0300

    elemento nav movido para componente Nav.js

---
+++ src/components/Nav.js
@@ -0,0 +1,27 @@
+function Nav() {
+    return (
+        <div className="nav-bar">
+            <nav>
+                <a href="#">Lorem Ipsum</a>
+
+                <ul>
+                    <li>
+                        <a href="#sobre">Sobre</a>
+                    </li>
+
+                    <li>
+                        <a href="#servicos">Serviços</a>
+                    </li>
+
+                    <li>
+                        <a href="#portfolio">Portfólio</a>
+                    </li>
+
+                    <li>
+                        <a href="#contato">Contato</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+    );
+}
