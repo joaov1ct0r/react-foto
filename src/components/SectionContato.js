d1aa2345753c52da50817772d88e93d4ee4ac4e2
Joao Victor <jov9051@outlook.com>
2022-02-07 19:08:00 -0300

    img adicionada no componente sectionContato

--- src/components/SectionContato.js
+++ src/components/SectionContato.js
@@ -4,7 +4,7 @@ function SectionContato() {
             <h2>Contato</h2>
             <p>Entre em contato conosco.</p>
             <div>
-                <img src="../assets/img/fone.png" alt="fone"></img>
+                <img src={require('../assets/img/fone.png')} alt="fone"></img>
                 <p>
                     <a href="tel:xx55555555">(xx) 5555-5555</a>
                 </p>
